29f63626d71fe9c2048d1d49980cfae96820b1c9
Tim Smart <tim.smart@arisechurch.com>
2021-05-06 11:43:29 +1200

    Add immutable.js

--- src/invite-tracking/invite-tracker.ts
+++ src/invite-tracking/invite-tracker.ts
@@ -1,5 +1,6 @@
-import { Bloc, BlocAction } from "@bloc-js/bloc";
+import { Bloc } from "@bloc-js/bloc";
 import { Client, Guild } from "discord.js";
+import { Map } from "immutable";
 import * as Rx from "rxjs";
 import * as RxO from "rxjs/operators";
 import * as DR from "../discord/rxjs";
@@ -10,24 +11,22 @@ export type TInviteSummary = {
   channel: string;
 };
 
-type TInviteMap = {
-  [inviteCode: string]: TInviteSummary;
-};
+export type TInviteMap = Map<string, TInviteSummary>;
 
-export type State = {
-  [guildId: string]: TInviteMap;
-};
-
-export type Action = BlocAction<State>;
+export type State = Map<string, TInviteMap>;
+export type Action = (
+  b: InviteTracker,
+  next: (s: State) => void,
+) => Promise<void>;
 
 export const init = (c: Client): Action => (b, next) =>
   DR.ready(c)
     .pipe(
       RxO.delay(1000),
       RxO.flatMap(() => c.guilds.cache.values()),
-      RxO.flatMap((guild) => updateGuild(guild)(b, next) as Promise<void>),
+      RxO.flatMap((guild) => updateGuild(guild)(b, next)),
     )
-    .toPromise() as Promise<void>;
+    .toPromise();
 
 export const updateGuild = (guild: Guild): Action => (b, next) =>
   Rx.from(guild.fetchInvites())
@@ -45,16 +44,16 @@ export const updateGuild = (guild: Guild): Action => (b, next) =>
         {} as TInviteMap,
       ),
       RxO.tap((invites) => {
-        next({
-          ...b.value,
-          [guild.id]: invites,
-        });
+        next(b.value.set(guild.id, invites));
       }),
     )
     .toPromise() as Promise<void>;
 
+export const removeGuild = (guildId: string): Action => async (b, next) =>
+  next(b.value.delete(guildId));
+
 export class InviteTracker extends Bloc<State> {
   constructor() {
-    super({});
+    super(Map());
   }
 }
--- src/invite-tracking/invites.ts
+++ src/invite-tracking/invites.ts
@@ -1,26 +1,35 @@
 import { Client, GuildMember } from "discord.js";
 import * as F from "fp-ts/function";
+import { sequenceT } from "fp-ts/lib/Apply";
+import * as O from "fp-ts/Option";
+import { List } from "immutable";
 import * as Rx from "rxjs";
 import * as RxO from "rxjs/operators";
 import * as Guilds from "../discord/guilds";
 import * as DR from "../discord/rxjs";
 import * as IT from "./invite-tracker";
 
+const usedInvites = (before: IT.TInviteMap, after: IT.TInviteMap) => {
+  return after.reduce((invites, invite, code) => {
+    const beforeUses = before.get(code)?.uses || 0;
+    const afterUses = after.get(code)?.uses || 0;
+    return beforeUses < afterUses ? invites.push(invite) : invites;
+  }, List<IT.TInviteSummary>());
+};
+
 const memberUsedInvite = (tracker: IT.InviteTracker) => (
   member: GuildMember,
-) => {
-  const before = tracker.value[member.guild.id];
+): Promise<List<IT.TInviteSummary>> => {
+  const before = O.fromNullable(tracker.value.get(member.guild.id));
 
   return tracker.next(IT.updateGuild(member.guild)).then(() => {
-    const after = tracker.value[member.guild.id];
+    const after = O.fromNullable(tracker.value.get(member.guild.id));
 
-    return Object.entries(after).reduce((invites, [code, invite]) => {
-      if (before[code].uses === invite.uses) {
-        return invites;
-      }
-
-      return [...invites, invite];
-    }, [] as IT.TInviteSummary[]);
+    return F.pipe(
+      sequenceT(O.option)(before, after),
+      O.map(([before, after]) => usedInvites(before, after)),
+      O.getOrElse(() => List()),
+    );
   });
 };
 
@@ -36,7 +45,7 @@ export const used$ = (client: Client) => {
     RxO.flatMap(([member]) =>
       Rx.zip(Rx.of(member), memberUsedInvite(inviteTracker)(member)),
     ),
-    RxO.filter(([_, invites]) => invites.length === 1),
-    RxO.map(([member, invites]) => F.tuple(member, invites[0])),
+    RxO.filter(([_, invites]) => invites.count() === 1),
+    RxO.map(([member, invites]) => F.tuple(member, invites.get(0)!)),
   );
 };
