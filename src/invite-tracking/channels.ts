a3267bcb5cff8ab3d30d60a20d15388400757964
Tim Smart <tim.smart@arisechurch.com>
2021-05-13 11:28:04 +1200

    Update droff to 0.6.0

--- src/invite-tracking/channels.ts
+++ src/invite-tracking/channels.ts
@@ -1,23 +1,22 @@
-import { APIChannel, APIRole } from "discord-api-types";
-import * as Arr from "fp-ts/Array";
+import { APIChannel, APIRole, Snowflake } from "discord-api-types";
+import { SnowflakeMap } from "droff/dist/gateway-utils/resources";
 import * as F from "fp-ts/function";
 import * as O from "fp-ts/Option";
 
-export const rolesFromTopic = (channels: APIChannel[], roles: APIRole[]) => (
-  channelId: string,
-) =>
-  F.pipe(
-    channels,
-    Arr.findFirst((c) => c.id === channelId),
-    O.chainNullableK((c) => c.topic),
-    O.chainNullableK((topic) => /\[\[(.*)\]\]/.exec(topic)),
-    O.map((matches) => matches[1].split(",").map((role) => role.trim())),
-    O.map(
-      (roleNames) =>
-        roleNames
-          .map((name) =>
-            roles.find((r) => r.name.toLowerCase() === name.toLowerCase()),
-          )
-          .filter((r) => !!r) as APIRole[],
-    ),
-  );
+export const rolesFromTopic =
+  (channels: SnowflakeMap<APIChannel>, roles: SnowflakeMap<APIRole>) =>
+  (channelId: string) =>
+    F.pipe(
+      O.fromNullable(channels.get(channelId as Snowflake)),
+      O.chainNullableK((c) => c.topic),
+      O.chainNullableK((topic) => /\[\[(.*)\]\]/.exec(topic)),
+      O.map((matches) => matches[1].split(",").map((role) => role.trim())),
+      O.map(
+        (roleNames) =>
+          roleNames
+            .map((name) =>
+              roles.find((r) => r.name.toLowerCase() === name.toLowerCase()),
+            )
+            .filter((r) => !!r) as APIRole[],
+      ),
+    );
--- src/invite-tracking/invite-roles.ts
+++ src/invite-tracking/invite-roles.ts
@@ -4,40 +4,34 @@ import * as F from "fp-ts/function";
 import * as O from "fp-ts/Option";
 import * as Rx from "rxjs";
 import * as RxO from "rxjs/operators";
-import * as Members from "../discord/members";
 import * as Channels from "./channels";
 import * as IT from "./invite-tracker";
 
-export const addRolesFromInvite = (c: Client) => (
-  input$: Rx.Observable<[GatewayGuildMemberAddDispatchData, IT.TInviteSummary]>,
-) =>
-  input$.pipe(
-    RxO.withLatestFrom(c.guilds$),
-    RxO.flatMap(([[member, invite], guilds]) =>
-      F.pipe(
-        Members.guild(guilds)(member),
-        O.fold(
-          () => Rx.EMPTY,
-          (guild) => Rx.of(F.tuple(member, invite, guild)),
+export const addRolesFromInvite =
+  (c: Client) =>
+  (
+    input$: Rx.Observable<
+      [GatewayGuildMemberAddDispatchData, IT.TInviteSummary]
+    >,
+  ) =>
+    input$.pipe(
+      c.withLatest(([member]) => member.guild_id),
+
+      // Get the roles from the invite
+      RxO.flatMap(([[member, { channel }], guildCtx]) =>
+        F.pipe(
+          Channels.rolesFromTopic(guildCtx!.channels, guildCtx!.roles)(channel),
+          O.fold(
+            () => Rx.EMPTY,
+            (roles) => Rx.combineLatest(Rx.of(member), roles),
+          ),
         ),
       ),
-    ),
 
-    // Get the roles from the invite
-    RxO.flatMap(([member, { channel }, guild]) =>
-      F.pipe(
-        Channels.rolesFromTopic(guild.channels!, guild.roles)(channel),
-        O.fold(
-          () => Rx.EMPTY,
-          (roles) => Rx.zip(Rx.of(member), roles),
-        ),
+      // Add the roles to the member
+      RxO.flatMap(([member, role]) =>
+        c
+          .putGuildMemberRole([member.guild_id, member.user!.id, role.id])
+          .then(() => F.tuple(member, role)),
       ),
-    ),
-
-    // Add the roles to the member
-    RxO.flatMap(([member, role]) =>
-      c
-        .putGuildMemberRole([member.guild_id, member.user!.id, role.id])
-        .then(() => F.tuple(member, role)),
-    ),
-  );
+    );
